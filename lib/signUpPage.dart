8e80b24cb191eeefdff448dc085d61aa1a161c27
sachithrakalani <kalanisathyangi@gmail.com>
2024-02-20 11:53:27 +0530

    Create Second Container in Text Fields

--- lib/signUpPage.dart
+++ lib/signUpPage.dart
@@ -13,21 +13,123 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
       body:Padding(
         padding: const EdgeInsets.only(top:24),
-        child: Column(
-          children: [
-            Container(
-              width:w ,
-              height: h * 0.3,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    'assets/images/signUp.jpg'
-                  ),  
-                )
+        child:SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width:w ,
+                height: h * 0.3,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(
+                      'assets/images/signUp.jpg'
+                    ),  
+                  )
+                ),
               ),
-            ) 
-          ],),
+              const SizedBox(height: 20),
+              Container(
+                margin: const EdgeInsets.only(left: 20,right: 20),
+                width: w,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Hello',
+                      style: TextStyle(
+                        fontSize: 70,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    const Text(
+                      'Sign into your account',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 10,
+                            offset: const Offset(1,1),
+                            color: Colors.grey.withOpacity(0.5)
+                          )
+                        ]
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                               width: 1.0
+                            )
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                              width: 1.0)
+                          )
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20,),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 10,
+                            offset: const Offset(1,1),
+                            color: Colors.grey.withOpacity(0.5)
+                          )
+                        ]
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                               width: 1.0
+                            )
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                              width: 1.0)
+                          )
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 25,),
+                    Row(
+                      children: [
+                        Expanded(child:Container(),),
+                        const Text(
+                          'Forgot your Password?',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.grey
+                          ),
+                        )
+                      ],
+                    )  
+                  ]),
+              ),
+              
+            ],
+          ),
+        ),
       )
     );
   }
